dd35726aca64280d7b0bbc6eea94b7d87146e6c8
Ramil Rahimov <ramil.rahimov2003@gmail.com>
2022-11-05 12:16:07 +0300

    second commit

--- DemoRakhimov4432/DemoRakhimov4432/Windows/ClientWindow.xaml.cs
+++ DemoRakhimov4432/DemoRakhimov4432/Windows/ClientWindow.xaml.cs
@@ -22,6 +22,44 @@ namespace DemoRakhimov4432.Windows
         public ClientWindow()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using(var db = new Model())
+            {
+                var products = db.Product.ToList();
+                foreach(var product in products)
+                {
+                    var mainPanel = new Grid();
+
+                    mainPanel.ColumnDefinitions.Add(new ColumnDefinition());
+                    mainPanel.ColumnDefinitions.Add(new ColumnDefinition());
+                    mainPanel.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(100) });
+
+                    var img = new Image() { Margin = new Thickness(10)};
+                    try
+                    {
+                        img.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + @"/Resorces/" + product.Image, UriKind.Absolute));
+                    }
+                    catch { }
+
+                    TextBlock txtDesc = new TextBlock();
+                    txtDesc.Text = product.Name + "\n" + product.Description + "\nПроизводитель: " + product.Manufacturer + "\nЦена: " + product.Price.ToString();
+
+                    TextBlock txtDisxcount = new TextBlock() { FontFamily = new FontFamily("Candara")};
+                    txtDisxcount.Text = product.Discount.ToString()+"%";
+                    Grid.SetColumn(img,0);
+                    Grid.SetColumn(txtDesc, 1);
+                    Grid.SetColumn(txtDisxcount, 2);
+                    mainPanel.Children.Add(img);
+                    mainPanel.Children.Add(txtDesc);
+                    mainPanel.Children.Add(txtDisxcount);
+                    mainPanelMain.Children.Add(mainPanel);
+
+                }
+            }
         }
     }
 }
